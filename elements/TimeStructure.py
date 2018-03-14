094b450ac175ad2fbb4a6f590f08097308fcff16
Kevin Frantz <mail@kevin-frantz.de>
2018-03-14 15:38:41 +0100

    Implemented TimeStructure

---
+++ elements/TimeStructure.py
@@ -0,0 +1,17 @@
+#A day containes multiple commits
+class Day:
+    commits = [];
+    datetime;
+    def __init__(self,datetime):
+        self.datetime = datetime;
+    def addCommit(self,commit):
+        self.commits.append(commit);
+
+#A week contines multiple days
+class Week:
+    days = [];
+    datetime;
+    def __init__(self,datetime):
+        self.datetime = datetime;
+    def addDay(self,day):
+        self.days.append(day);
