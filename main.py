3719d100ac6fe9e43d080c777753be38964ab7ae
Kevin Frantz <mail@kevin-frantz.de>
2018-03-15 12:13:49 +0100

    Implemented submodule latex vorlage

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import Core;
 from pprint import pprint
 #gitPath = input("Please type in the path of the git-repository:");
-gitPath = r'/home/kevinfrantz/repositories/kevinfrantz.github/berichtsheft-latex-generator';
+gitPath = r'/home/kevinfrantz/repositories/kevinfrantz.github/displaylink-debian';
 print("The programm will use \"{0}\" as repository path.".format(gitPath));
 core = Core.Core(gitPath);
 core.exportToCSV();
