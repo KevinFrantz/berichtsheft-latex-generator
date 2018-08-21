48fadb3cbfb518aae5444e535663788cd9bdb351
Kevin Frantz <mail@kevin-frantz.de>
2018-08-21 18:54:58 +0200

    Added basic filters

--- elements/Commit.py
+++ elements/Commit.py
@@ -3,5 +3,7 @@ class Commit:
     datetime;
     message  = '';
     def __init__(self,message:str,datetime):
-        self.message  = message;
+        self.setMessage(message)
         self.datetime = datetime;
+    def setMessage(self,message:str):
+        self.message = message
---
+++ elements/Filter.py
@@ -0,0 +1,18 @@
+import re;
+class Filter(object):
+    """
+    This class filters text
+    """
+    commit_list = []
+    def __init__(self,commit_list):
+        self.commit_list = commit_list
+    def routine(self):
+        for commit in self.commit_list:
+            commit.setMessage(self.escape(commit.message))
+    def escape(self,text):
+        words = {
+            'refs #':'ticket',
+        }
+        for word in words.keys():
+            text = re.sub(word,words[word],text)
+        return text
