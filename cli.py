af84e9439329f05f34082056a899b3687c57fbef
Kevin Frantz <mail@kevin-frantz.de>
2018-08-22 11:42:02 +0200

    Implemented random file-up setters

--- cli.py
+++ cli.py
@@ -19,6 +19,7 @@ class Main(object):
         self.user.setFirma(self.args.company)
         self.user.setCountStart(self.args.count_start)
         self.user.setLanguage(self.args.translation)
+        self.user.setRandomFileUp(self.args.random_fill_up)
     def parser(self):
         parser = argparse.ArgumentParser(description='Generates reports based on a git repository')
         parser.add_argument('--path', help='The path to the git repository')
@@ -28,5 +29,6 @@ class Main(object):
         parser.add_argument('--company', help='The company')
         parser.add_argument('--count-start', default=1, help='Number from which the counting should start')
         parser.add_argument('--translation', default='', help='Language to which the reports should be translated trough')
+        parser.add_argument('--random-fill-up', default='', help='Time to which logs should be filled up with random commits')
         self.args = parser.parse_args()
 Main()
--- elements/User.py
+++ elements/User.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class User:
     '''
         This class represents the user
@@ -12,6 +13,7 @@ class User:
         self.firma = ''
         self.language = ''
         self.count_start = 0
+        self.random_fill_up_datetime = False;
     def setFirstName(self,first_name:str):
         self.first_name = first_name
     def setLastName(self,last_name:str):
@@ -26,6 +28,9 @@ class User:
         self.language = language
     def setCountStart(self,count_start:int):
         self.count_start = count_start
+    def setRandomFileUp(self,random_fill_up:str):
+        if random_fill_up != '':
+            self.random_fill_up_datetime = datetime.strptime(random_fill_up, '%Y-%m-%d')
     @property
     def fullName(self):
         return self.first_name + " " + self.last_name
